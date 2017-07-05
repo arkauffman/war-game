f0892bf18cbe33cfec514f6324e8b4877772c72f
Alexandra Kauffman <arkauffma@gmail.com>
2017-07-05 11:04:34 -0700

    updated styling for game

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 /*----- app's state (variables) -----*/
-var cards, playerOneArr, playerTwoArr, playOneLength, playTwoLength;
+var cards, playerOneArr, playerTwoArr;
 
 /*----- event listeners -----*/
 $('#flip').on('click', flipCard);
@@ -11,21 +11,7 @@ $('#reset').on('click', init);
 
 /*----- functions -----*/
 init();
-
-function render() {
-    // display correct card
-    $('#image1').attr('src', `${playerOneArr[0].cardImage}`);
-    $('#image1').css('background-color', '#f4f5f7');
-    $('#image2').attr('src', `${playerTwoArr[0].cardImage}`);
-    $('#image2').css('background-color', '#f4f5f7');
-
-    // display count
-    $('#count1').html(`${playerOneArr.length}`);
-    $('#count2').html(`${playerTwoArr.length}`);
-
-    // handle display when card values are equal
-
-}
+// render();
 
 function init() {
 
@@ -243,27 +229,49 @@ function init() {
     ];
 
     shuffle(cards);
-
     playerOneArr = cards.slice(0, (cards.length/2));
     playerTwoArr = cards.slice((cards.length/2), cards.length);
 
-    playOneLength = playerOneArr.length;
-    playTwoLength = playerTwoArr.length;
+    // playerOneArr = cards.slice(0, 1);
+    // playerTwoArr = cards.slice(10, 12);
 
     $('#image1').attr('src', ``);
     $('#image2').attr('src', ``);
     $('#count1').html(`${playerOneArr.length}`);
     $('#count2').html(`${playerTwoArr.length}`);
+}
 
-    // $('#war').remove();
+function render() {
+    if (playerOneArr.length === 0) {
+        console.log('PLAYER 1 LOST');
+        $('#count2').html("WINNER!").css('color', '#4682b4');
+        $('#count1').html('0');
+        $('#war').off('click', war);
+        $('#flip').off('click', flipCard);
+    } else if (playerTwoArr.length === 0) {
+        console.log('PLAYER 2 LOST')
+        $('#count1').html("WINNER!").css('color', '#4682b4');
+        $('#count2').html('0');
+        $('#war').off('click', war);
+        $('#flip').off('click', flipCard);
+    } else {
+        // display correct card
+        $('#image1').attr('src', `${playerOneArr[0].cardImage}`);
+        $('#image1').css('background-color', '#f4f5f7');
+        $('#image2').attr('src', `${playerTwoArr[0].cardImage}`);
+        $('#image2').css('background-color', '#f4f5f7');
+
+        // display count
+        $('#count1').html(`${playerOneArr.length}`);
+        $('#count2').html(`${playerTwoArr.length}`);
+    }
 }
 
 function flipCard() {
     render();
     // no longer able to flip card if either player has no cards left
-    if (playerOneArr.length <= 0 || playerTwoArr.length <= 0) {
-        return;
-    }
+    if (playerOneArr.length <= 0 || playerTwoArr.length <= 0) return;
+ 
     // check which card's value is higher
     if (playerOneArr[0].value > playerTwoArr[0].value) {
         // first move first card on pile to back of deck
@@ -282,308 +290,88 @@ function flipCard() {
     } else if (playerOneArr[0].value === playerTwoArr[0].value) {
         $('#war').on('click', war);
         $('#flip').off('click', flipCard);
-
-        // var warButton = ("<button id='war'>War</button>");
-        // $('.button-format').append(warButton);
-        //war();
-    }
-}
-
-function warCardLength() {
-
-    if (playOneLength >= 4) {
-        if (playTwoLength >= 4) {
-            playOneLength = 4;
-            playTwoLength = 4;
-        } else if (playTwoLength >= 3) {
-            playOneLength = 4;
-            playTwoLength = 3;
-        } else if (playTwoLength >= 2) {
-            playOneLength = 4;
-            playTwoLength = 2;
-        } else if (playTwoLength >= 1) {
-            playOneLength = 4;
-            playTwoLength = 1;
-        }
-    } else if (playOneLength >= 3) {
-        if (playTwoLength >= 4) {
-            playOneLength = 3;
-            playTwoLength = 4;
-        } else if (playTwoLength >= 3) {
-            playOneLength = 3;
-            playTwoLength = 3;
-        } else if (playTwoLength >= 2) {
-            playOneLength = 3;
-            playTwoLength = 2;
-        } else if (playTwoLength >= 1) {
-            playOneLength = 3;
-            playTwoLength = 1;
-        }
-    } else if (playOneLength >= 2) {
-        if (playTwoLength >= 4) {
-            playOneLength = 2;
-            playTwoLength = 4;
-        } else if (playTwoLength >= 3) {
-            playOneLength = 2;
-            playTwoLength = 3;
-        } else if (playTwoLength >= 2) {
-            playOneLength = 2;
-            playTwoLength = 2;
-        } else if (playTwoLength >= 1) {
-            playOneLength = 2;
-            playTwoLength = 1;
-        }
-    } else if (playeOneLength >= 1) {
-        if (playTwoLength >= 4) {
-            playOneLength = 1;
-            playTwoLength = 4;
-        } else if (playTwoLength >= 3) {
-            playOneLength = 1;
-            playTwoLength = 3;
-        } else if (playTwoLength >= 2) {
-            playOneLength = 1;
-            playTwoLength = 2;
-        } else if (playTwoLength >= 1) {
-            playOneLength = 1;
-            playTwoLength = 1;
-        }
     }
 }
 
 function war () {
     console.log('war is declared')
+    var playVal = 0;
+    if (playerOneArr.length >= 4 && playerTwoArr.length >= 4) {
+        playVal = 4;
+    } else if (playerOneArr.length >= 3 && playerTwoArr.length >= 3) {
+        playVal = 3;
+    } else if (playOneArr.length >= 2 && playerTwoArr.length >= 2) {
+        playVal = 2;
+    } else if (playerOneArr.length >= 1 && playerTwoArr.length >= 1) {
+        playVal = 1;
+    }
+
     if (playerOneArr[0].value === playerTwoArr[0].value) {
-        if (playerOneArr.length >= 4 && playerTwoArr.length >= 4) {
-            $('#image1').attr('src', `${playerOneArr[4].cardImage}`);
-                $('#image1').css('background-color', '#f4f5f7');
-                $('#image2').attr('src', `${playerTwoArr[4].cardImage}`);
-                $('#image2').css('background-color', '#f4f5f7');
+        if (playerOneArr.length >= playVal && playerTwoArr.length >= playVal) {
+            $('#image1').attr('src', `${playerOneArr[playVal].cardImage}`);
+            $('#image1').css('background-color', '#f4f5f7');
+            $('#image2').attr('src', `${playerTwoArr[playVal].cardImage}`);
+            $('#image2').css('background-color', '#f4f5f7');
             
-                console.log('player 1');
-                console.log(playerOneArr[0]);
-                console.log(playerOneArr[1]);
-                console.log(playerOneArr[2]);
-                console.log(playerOneArr[3]);
-                console.log(playerOneArr[4]);
+            console.log('player 1');
+            console.log(playerOneArr[0]);
+            console.log(playerOneArr[1]);
+            console.log(playerOneArr[2]);
+            console.log(playerOneArr[3]);
+            console.log(playerOneArr[4]);
 
-                console.log('player 2');
-                console.log(playerTwoArr[0]);
-                console.log(playerTwoArr[1]);
-                console.log(playerTwoArr[2]);
-                console.log(playerTwoArr[3]);
-                console.log(playerTwoArr[4]);
+            console.log('player 2');
+            console.log(playerTwoArr[0]);
+            console.log(playerTwoArr[1]);
+            console.log(playerTwoArr[2]);
+            console.log(playerTwoArr[3]);
+            console.log(playerTwoArr[4]);
 
-                if (playerOneArr[4].value > playerTwoArr[4].value) {
+            if (playerOneArr[playVal].value > playerTwoArr[playVal].value) {
 
-                    var takeOne = playerOneArr.splice(0, 5);
-                    var takeTwo = playerTwoArr.splice(0, 5);
+                var takeOne = playerOneArr.splice(0, playVal + 1);
+                var takeTwo = playerTwoArr.splice(0, playVal + 1);
 
-                    var newArr = takeOne.concat(takeTwo);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerOneArr.concat(newArr);
-                    playerOneArr = newArrUpdate;
+                var newArr = takeOne.concat(takeTwo);
+                console.log('CONCAT');
+                console.log(newArr);
+                var newArrUpdate = playerOneArr.concat(newArr);
+                playerOneArr = newArrUpdate;
 
-                    console.log('player 1 post');
-                    console.log(playerOneArr);
+                console.log('player 1 post');
+                console.log(playerOneArr);
+                console.log('player 2 lost post');
+                console.log(playerTwoArr);
 
-                    console.log('player 2 lost post');
-                    console.log(playerTwoArr);
+            } else if (playerOneArr[playVal].value < playerTwoArr[playVal].value) {
+                var takeTwo = playerTwoArr.splice(0, playVal + 1);
+                var takeOne = playerOneArr.splice(0, playVal + 1);
 
-                } else if (playerOneArr[4].value < playerTwoArr[4].value) {
+                var newArr = takeTwo.concat(takeOne);
+                console.log('CONCAT');
+                console.log(newArr);
+                var newArrUpdate = playerTwoArr.concat(newArr);
+                playerTwoArr = newArrUpdate;
 
-                    var takeTwo = playerTwoArr.splice(0, 5);
-                    var takeOne = playerOneArr.splice(0, 5);
-
-                    var newArr = takeTwo.concat(takeOne);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerTwoArr.concat(newArr);
-                    playerTwoArr = newArrUpdate;
-
-                    console.log('player 2 post');
-                    console.log(playerTwoArr);
-
-                    console.log('player 1 lost post');
-                    console.log(playerOneArr);
-                } else if (playerOneArr[4].value === playerTwoArr[4].value) {
-                    war();
-                }
-
-            } else if (playerOneArr.length >= 3 && playerTwoArr.length >= 3) {
-
-                $('#image1').attr('src', `${playerOneArr[3].cardImage}`);
-                $('#image1').css('background-color', '#f4f5f7');
-                $('#image2').attr('src', `${playerTwoArr[3].cardImage}`);
-                $('#image2').css('background-color', '#f4f5f7');
-            
-                console.log('player 1');
-                console.log(playerOneArr[0]);
-                console.log(playerOneArr[1]);
-                console.log(playerOneArr[2]);
-                console.log(playerOneArr[3]);
-
-                console.log('player 2');
-                console.log(playerTwoArr[0]);
-                console.log(playerTwoArr[1]);
-                console.log(playerTwoArr[2]);
-                console.log(playerTwoArr[3]);
-
-                if (playerOneArr[3].value > playerTwoArr[3].value) {
-
-                    var takeOne = playerOneArr.splice(0, 4);
-                    var takeTwo = playerTwoArr.splice(0, 4);
-
-                    var newArr = takeOne.concat(takeTwo);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerOneArr.concat(newArr);
-                    playerOneArr = newArrUpdate;
-
-                    console.log('player 1 post');
-                    console.log(playerOneArr);
-
-                    console.log('player 2 lost post');
-                    console.log(playerTwoArr);
-
-                } else if (playerOneArr[3].value < playerTwoArr[3].value) {
-
-                    var takeTwo = playerTwoArr.splice(0, 4);
-                    var takeOne = playerOneArr.splice(0, 4);
-
-                    var newArr = takeTwo.concat(takeOne);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerTwoArr.concat(newArr);
-                    playerTwoArr = newArrUpdate;
-
-                    console.log('player 2 post');
-                    console.log(playerTwoArr);
-
-                    console.log('player 1 lost post');
-                    console.log(playerOneArr);
-                } else if (playerOneArr[3].value === playerTwoArr[3].value) {
-                    war();
-                }
-
-            } else if (playerOneArr.length >= 2 && playerTwoArr.length >= 2) {
-                $('#image1').attr('src', `${playerOneArr[2].cardImage}`);
-                $('#image1').css('background-color', '#f4f5f7');
-                $('#image2').attr('src', `${playerTwoArr[2].cardImage}`);
-                $('#image2').css('background-color', '#f4f5f7');
-            
-                console.log('player 1');
-                console.log(playerOneArr[0]);
-                console.log(playerOneArr[1]);
-                console.log(playerOneArr[2]);
-
-                console.log('player 2');
-                console.log(playerTwoArr[0]);
-                console.log(playerTwoArr[1]);
-                console.log(playerTwoArr[2]);
-
-                if (playerOneArr[2].value > playerTwoArr[2].value) {
-
-                    var takeOne = playerOneArr.splice(0, 3);
-                    var takeTwo = playerTwoArr.splice(0, 3);
-
-                    var newArr = takeOne.concat(takeTwo);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerOneArr.concat(newArr);
-                    playerOneArr = newArrUpdate;
-
-                    console.log('player 1 post');
-                    console.log(playerOneArr);
-
-                    console.log('player 2 lost post');
-                    console.log(playerTwoArr);
-
-                } else if (playerOneArr[2].value < playerTwoArr[2].value) {
-
-                    var takeTwo = playerTwoArr.splice(0, 3);
-                    var takeOne = playerOneArr.splice(0, 3);
-
-                    var newArr = takeTwo.concat(takeOne);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerTwoArr.concat(newArr);
-                    playerTwoArr = newArrUpdate;
-
-                    console.log('player 2 post');
-                    console.log(playerTwoArr);
-
-                    console.log('player 1 lost post');
-                    console.log(playerOneArr);
-                } else if (playerOneArr[2].value === playerTwoArr[2].value) {
-                    war();
-                }
-            } else if (playerOneArr.length >= 1 && playerTwoArr.length >= 1) {
-                $('#image1').attr('src', `${playerOneArr[1].cardImage}`);
-                $('#image1').css('background-color', '#f4f5f7');
-                $('#image2').attr('src', `${playerTwoArr[1].cardImage}`);
-                $('#image2').css('background-color', '#f4f5f7');
-            
-                console.log('player 1');
-                console.log(playerOneArr[0]);
-                console.log(playerOneArr[1]);
-
-                console.log('player 2');
-                console.log(playerTwoArr[0]);
-                console.log(playerTwoArr[1]);
-
-                if (playerOneArr[1].value > playerTwoArr[1].value) {
-
-                    var takeOne = playerOneArr.splice(0, 2);
-                    var takeTwo = playerTwoArr.splice(0, 2);
-
-                    var newArr = takeOne.concat(takeTwo);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerOneArr.concat(newArr);
-                    playerOneArr = newArrUpdate;
-
-                    console.log('player 1 post');
-                    console.log(playerOneArr);
-
-                    console.log('player 2 lost post');
-                    console.log(playerTwoArr);
-
-                } else if (playerOneArr[1].value < playerTwoArr[1].value) {
-
-                    var takeTwo = playerTwoArr.splice(0, 2);
-                    var takeOne = playerOneArr.splice(0, 2);
-
-                    var newArr = takeTwo.concat(takeOne);
-                    console.log('CONCAT');
-                    console.log(newArr);
-                    var newArrUpdate = playerTwoArr.concat(newArr);
-                    playerTwoArr = newArrUpdate;
-
-                    console.log('player 2 post');
-                    console.log(playerTwoArr);
-
-                    console.log('player 1 lost post');
-                    console.log(playerOneArr);
-                } else if (playerOneArr[1].value === playerTwoArr[1].value) {
-                    war();
-                }
-
-            }
-
-        } 
+                console.log('player 2 post');
+                console.log(playerTwoArr);
+                console.log('player 1 lost post');
+                console.log(playerOneArr);
+            } else if (playerOneArr[playVal].value === playerTwoArr[playVal].value) {
+                war();
+            } 
+        }
+    } 
     $('#flip').on('click', flipCard);
     $('#war').off('click', war);
 }
 
 function shuffle(arr) {
     var currIndex = arr.length, temp, randIndex;
-
     // loop the array and randomize
     while (currIndex > 0) {
         randIndex = Math.floor(Math.random() * currIndex);
         currIndex -= 1;
-
         //swap
         temp = arr[currIndex];
         arr[currIndex] = arr[randIndex];
